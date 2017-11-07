9d9f85c2d2b4bff154d033f4f70b13373918b975
JalIrani <jaljasonirani@gmail.com>
2017-11-07 12:40:41 -0500

    drink filter completed

---
+++ RollingGrill/Drink/Filter/View/DrinkFilterViewController.swift
@@ -0,0 +1,63 @@
+//
+//  DrinkFilterViewController.swift
+//  RollingGrill
+//
+//  Created by Jal Irani on 11/7/17.
+//  Copyright © 2017 Apptzr. All rights reserved.
+//
+
+import UIKit
+
+class DrinkFilterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var sectionsFilter = [String]()
+    var selectedFilter = "None"
+    var selectedIndex = -1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sectionsFilter.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "drinkFilterCell", for: indexPath) as UITableViewCell
+        print(sectionsFilter[indexPath.row])
+        cell.textLabel?.text = sectionsFilter[indexPath.row]
+        if (indexPath.row == selectedIndex) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            print("Row we checked: \(indexPath.row)")
+            if cell.accessoryType == .checkmark {
+                selectedFilter = "None"
+                selectedIndex = -1
+                cell.accessoryType = .none
+            } else {
+                cell.accessoryType = .checkmark
+                selectedFilter = sectionsFilter[indexPath.row]
+                selectedIndex = indexPath.row
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+    }
+}
--- RollingGrill/Drink/View/DrinkViewController.swift
+++ RollingGrill/Drink/View/DrinkViewController.swift
@@ -12,9 +12,30 @@ class DrinkViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     @IBOutlet weak var drinkTableView: UITableView!
     
-    var drinkItemArray = ["Tomato Caprese Skewers", "Bacon and BBQ Infused Chicken Kabobs", "Garlic Glazed Shrimp Kabobs", "Shrimp Cocktail", "Veggie Tray", "Cheese Tray", "Fruit Salad Platter"]
-    var drinkPriceArray = ["49.95", "79.95", "99.95", "16.95", "34.95", "49.95", "39.95"]
-    var drinkDescriptionArray = ["Feeds up to 30 people", "Feeds up to 30 people", "Feeds up to 30 people", "Served with cocktail sauce and lemon. (Priced per pound)", "Feeds up to 30 people", "Serves 20-40 people", "Feeds up to 30 people"]
+    var filterSection = ["None"]
+    var filterSectionIndex = -1
+    var drinkItemsSectionedFilter = [[String]]()
+    var drinkPricesSectionedFilter = [[String]]()
+    var drinkDescriptionsSectionedFilter = [[String]]()
+    
+    var sections = ["Sandwiches", "cyclops"]
+    
+    var drinkItemsSectioned = [
+        ["Tomato Caprese Skewers", "Bacon and BBQ Infused Chicken Kabobs", "Garlic Glazed Shrimp Kabobs", "Shrimp Cocktail", "Veggie Tray", "Cheese Tray", "Fruit Salad Platter", "Maryland Style Crab Balls", "Maryland Style Crab Balls", "Maryland Style Crab Balls", "Maryland Style Crab Balls", "Maryland Style Crab Balls", "Maryland Style Crab Balls"],
+        ["poop"]
+    ]
+    var drinkPricesSectioned = [
+        ["49.95", "79.95", "99.95", "16.95", "34.95", "49.95", "39.95", "55.00", "110.00", "210.00", "111.00", "222.00", "420.00"],
+        ["3.00"]
+    ]
+    var drinkDescriptionsSectioned = [
+        ["Feeds up to 30 people", "Feeds up to 30 people", "Feeds up to 30 people", "Served with cocktail sauce and lemon. (Priced per pound)", "Feeds up to 30 people", "Serves 20-40 people", "Feeds up to 30 people", "25ct- 1 oz. Balls\nServed with cocktail, tarter, and lemon", "50ct- 1 oz. Balls\nServed with cocktail, tarter, and lemon", "100ct- 1 oz. Balls\nServed with cocktail, tarter, and lemon", "25ct- 2 oz. Balls\nServed with cocktail, tarter, and lemon", "50ct- 2 oz. Balls\nServed with cocktail, tarter, and lemon", "100ct- 2 oz. Balls\nServed with cocktail, tarter, and lemon"],
+        ["get some"]
+    ]
+    
+    var drinkItemArray = [String]()
+    var drinkPriceArray = [String]()
+    var drinkDescriptionArray = [String]()
     
     fileprivate var filteredDrinkItem = [String]()
     fileprivate var filteredDrinkPrice = [String]()
@@ -34,25 +55,64 @@ class DrinkViewController: UIViewController, UITableViewDelegate, UITableViewDat
         self.navigationItem.searchController = search
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        print("In view will appear:" + filterSection[0])
+        if self.filterSection[0] != "None" {
+            drinkItemsSectionedFilter.insert(self.drinkItemsSectioned[filterSectionIndex], at: 0)
+            drinkPricesSectionedFilter.insert(self.drinkPricesSectioned[filterSectionIndex], at: 0)
+            drinkDescriptionsSectionedFilter.insert(self.drinkDescriptionsSectioned[filterSectionIndex], at: 0)
+            drinkItemArray = Array(drinkItemsSectionedFilter.joined())
+            drinkPriceArray = Array(drinkPricesSectionedFilter.joined())
+            drinkDescriptionArray = Array(drinkDescriptionsSectionedFilter.joined())
+            print(drinkItemArray)
+        } else {
+            drinkItemArray = Array(drinkItemsSectioned.joined())
+            drinkPriceArray = Array(drinkPricesSectioned.joined())
+            drinkDescriptionArray = Array(drinkDescriptionsSectioned.joined())
+            drinkItemsSectionedFilter = [[String]]()
+            drinkPricesSectionedFilter = [[String]]()
+            drinkDescriptionsSectionedFilter = [[String]]()
+        }
+        drinkTableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.filterring ? self.filteredDrinkItem.count : drinkItemArray.count
+        if self.filterSection[0] != "None" {
+            return self.filterring ? self.filteredDrinkItem.count : drinkItemsSectionedFilter[section].count
+        } else {
+            return self.filterring ? self.filteredDrinkItem.count : drinkItemsSectioned[section].count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "drinkCell", for: indexPath) as! DrinkTableViewCell
-        if self.filterring {
-            cell.drinkItemLabel.text = self.filteredDrinkItem[indexPath.row]
-            cell.drinkPriceLabel.text = self.filteredDrinkPrice[indexPath.row]
-            cell.drinkDescriptionLabel.text = self.filteredDrinkDescription[indexPath.row]
+        if self.filterSection[0] != "None" {
+            if self.filterring {
+                //self.items[indexPath.section][indexPath.row]
+                cell.drinkItemLabel.text = self.filteredDrinkItem[indexPath.row]
+                cell.drinkPriceLabel.text = self.filteredDrinkPrice[indexPath.row]
+                cell.drinkDescriptionLabel.text = self.filteredDrinkDescription[indexPath.row]
+            } else {
+                cell.drinkItemLabel.text = drinkItemsSectionedFilter[indexPath.section][indexPath.row]
+                cell.drinkPriceLabel.text = "$" + drinkPricesSectionedFilter[indexPath.section][indexPath.row]
+                cell.drinkDescriptionLabel.text = drinkDescriptionsSectionedFilter[indexPath.section][indexPath.row]
+            }
         } else {
-            cell.drinkItemLabel.text = drinkItemArray[indexPath.row]
-            cell.drinkPriceLabel.text = "$" + drinkPriceArray[indexPath.row]
-            cell.drinkDescriptionLabel.text = drinkDescriptionArray[indexPath.row]
+            if self.filterring {
+                //self.items[indexPath.section][indexPath.row]
+                cell.drinkItemLabel.text = self.filteredDrinkItem[indexPath.row]
+                cell.drinkPriceLabel.text = self.filteredDrinkPrice[indexPath.row]
+                cell.drinkDescriptionLabel.text = self.filteredDrinkDescription[indexPath.row]
+            } else {
+                cell.drinkItemLabel.text = drinkItemsSectioned[indexPath.section][indexPath.row]
+                cell.drinkPriceLabel.text = "$" + drinkPricesSectioned[indexPath.section][indexPath.row]
+                cell.drinkDescriptionLabel.text = drinkDescriptionsSectioned[indexPath.section][indexPath.row]
+            }
         }
         
         cell.drinkItemLabel.textColor? = .itemName
@@ -67,6 +127,43 @@ class DrinkViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableViewAutomaticDimension
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if self.filterring || self.filterSection[0] != "None" {
+            return 1
+        } else {
+            return sections.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if self.filterring {
+            return "Filtered Results"
+        } else if self.filterSection[0] != "None" {
+            return filterSection[0]
+        }
+        else {
+            return sections[section]
+        }
+    }
+    
+    @IBAction func unwindFilterDrink(_ sender: UIStoryboardSegue) {
+        if sender.source is DrinkFilterViewController {
+            if let filterVC = sender.source as? DrinkFilterViewController {
+                self.filterSection[0] = filterVC.selectedFilter
+                self.filterSectionIndex = filterVC.selectedIndex
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "filterSectionsDrink" {
+            if let secondController = segue.destination as? DrinkFilterViewController {
+                print("In prepare for segue \(sections)")
+                secondController.sectionsFilter = self.sections
+            }
+        }
     }
 }
 
